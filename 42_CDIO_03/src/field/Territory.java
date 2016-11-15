8d331cbff0386c87c5823739efa93bce2cf88055
Jimmy <tippy@DESKTOP-F71BF8G.eduroam.wireless.dtu.dk>
2016-11-15 15:57:54 +0100

    Første version af territory - mangler landOnField

--- 42_CDIO_03/src/field/Territory.java
+++ 42_CDIO_03/src/field/Territory.java
@@ -1,5 +1,38 @@
 package field;
 
-public class Territory {
+public class Territory extends Ownable {
 
+	private int rent;
+	
+	public Territory(int price, int rent)
+	{
+		super(price);
+		this.rent = rent;
+	}
+	
+	public int getRent()
+	{
+		return rent;
+	}
+	
+	public  void landOnField(Player p)
+	{
+		Player owner = this.getOwner();
+		if(owner==null)
+		{
+			//Hvis feltet ingen ejer har
+			//Skal have mulighed for at købe
+		}
+		else if(owner==p)
+		{
+			//Hvis spilleren ejer feltet
+			//Sker der ikke noget
+		}
+		else
+		{
+			//Hvis spilleren ikke ejer feltet
+			//Betaler automatisk renten
+			//owner skal så have pengene
+		}
+	}
 }
