dd95228f305f052642f773ac1812e0b532901223
lasse <lassejensen213@gmail.com>
2016-11-16 08:58:21 +0100

    Adding Jimmys territory

--- 42_CDIO_03/src/field/Territory.java
+++ 42_CDIO_03/src/field/Territory.java
@@ -1,5 +1,50 @@
 package field;
 
-public class Territory {
+import player.Player;
 
+public class Territory extends Ownable {
+
+	private int rent;
+	
+	public Territory(int price, int rent)
+	{
+		super(price);
+		this.rent = rent;
+	}
+	
+	public int getRent()
+	{
+		return rent;
+	}
+	
+	public  void landOnField(Player p)
+	{
+		Player owner = this.getOwner();
+		if(owner==null && p.getAccount().getBalance()>super.getPrice())
+		{
+			//Hvis feltet ingen ejer har og spilleren har penge nok
+			//Skal have mulighed for at købe
+			String input = GUI.getUserSelection(new String[]{"Buy: "+this.getPrice(),"Skip"});
+			if(input.equals("Buy: "+this.getPrice()))
+			{
+				this.setOwner(p);
+				p.getAccount().deposit(-this.getPrice());
+			}
+		}
+		else if(owner==p)
+		{
+			//Hvis spilleren ejer feltet
+			//Sker der ikke noget
+			//dette skal være tomt
+		}
+		else
+		{
+			//Hvis spilleren ikke ejer feltet
+			//Betaler automatisk renten
+			//owner skal så have pengene
+			
+			p.getAccount().deposit(-rent);
+			owner.getAccount().deposit(rent);
+		}
+	}
 }
