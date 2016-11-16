926bd4580bb641442ee16b8a4dbeb2090be14627
lasse <lassejensen213@gmail.com>
2016-11-16 09:29:10 +0100

    Adding Michaels Refuge

--- 42_CDIO_03/src/field/Tax.java
+++ 42_CDIO_03/src/field/Tax.java
@@ -15,24 +15,34 @@ public class Tax extends Field{
 	@Override
 	public void landOnField(Player player)
 	{
-		
-		int pos = player.getPlayerPos();
-		if (pos == 3)
+		if (taxRate == 0)
 		{
 			GUI.showMessage("Tax message 2000 coins");
 			player.getAccount().withdraw(taxAmount);
 			
 		}
-		else if (pos == 21) 
+		else
 		{
-			int percentage = player.getAccount().getBalance()*this.taxRate;
+			int percentage = player.getAccount().getBalance()*taxRate/100;
 			String procent = "10%: " + percentage;
 			String options[] = {"4000",procent};
-			GUI.showMessage("Valg 4000 eller 10% af balance");
-			GUI.getUserSelection("Vælg", options);
+			String input = GUI.getUserSelection("Valg: 4000 eller 10% af balance", options);
+			
+			if(input.equals(options[0]))
+				{
+					player.getAccount().withdraw(taxAmount);
+				}
+			else if(input.equals(options[1]))
+			{
+				player.getAccount().withdraw(percentage);
+			}
+		}
 		}
 		
-	}
+		
+			
+		
+	
 	
 	
 	
