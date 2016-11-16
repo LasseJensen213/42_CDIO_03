77262818731493c3619dfa1c546def62f44ba427
soelberg484 <soelberg484@users.noreply.github.com>
2016-11-16 08:58:27 +0100

    Add files via upload

--- 42_CDIO_03/src/field/LaborCamp.java
+++ 42_CDIO_03/src/field/LaborCamp.java
@@ -1,5 +1,43 @@
 package field;
 
-public class LaborCamp {
+import desktop_resources.GUI;
+import player.Player;
 
+//Her skal man også betale en afgift til ejeren.
+//Beløbet bestemmes ved at slå med terningerne og gange resultatet med 100. 
+//Dette tal skal så ganges med antallet af Labor camps med den samme ejer. 
+
+public class LaborCamp extends Ownable {
+
+	private int baseRent;
+	
+	@Override
+	public void landOnField(Player player){
+		if(super.getOwner() == null && player.getAccount().getBalance()>this.getPrice()){
+			String input = GUI.getUserSelection("Do You want to buy this LaborCamp? Price is: "+ this.getPrice(), new String[]{"buy", "skip"});
+			if(input.equals("buy")){
+				this.setOwner(player);
+				player.getAccount().deposit(-this.getPrice());
+			}
+			
+			
+		}
+		else if(this.getOwner()==player){
+			
+		}
+		else{
+			int diceTotal = player.getDiceTotal();
+			int nLaborCampsOwned = this.getOwner().getLaborCamps();
+			player.getAccount().deposit(-(baseRent*diceTotal*nLaborCampsOwned));
+			this.getOwner().getAccount().deposit(baseRent*diceTotal*nLaborCampsOwned);
+		}
+			
+		
+		
+		
+	}
+	
+	
+
+	
 }
