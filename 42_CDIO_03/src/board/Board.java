80c2207c0a21a180208907f3a6720372f9ff1b90
lasse <lassejensen213@gmail.com>
2016-11-18 18:00:01 +0100

    Added stringbanks for Territory, Fleet, Tax, Laborcamp, refuge.

--- 42_CDIO_03/src/board/Board.java
+++ 42_CDIO_03/src/board/Board.java
@@ -3,54 +3,146 @@ package board;
 import java.awt.Color;
 import field.Field;
 import field.Territory;
+import field.Fleet;
+import field.LaborCamp;
+import field.Refuge;
+import field.Tax;
 
 import stringbanks.Territory_Stringbank;
+import stringbanks.Fleet_Stringbank;
+import stringbanks.Tax_Stringbank;
+import stringbanks.LaborCamp_Stringbank;
+import stringbanks.Refuge_Stringbank;
+
 public class Board {
 
 	//Field array
 	int fields[] = new int[40];	
-	
+
 	//GUI Colours
 	Color bgColors[] = new Color[40];
 	Color fgColors[] = new Color[40];
 
 	/**
 	 * Describes the fields in use in the GUI.
-	*/
+	 */
 	int fieldsInUse[] = {0,2,4,6,8,10,11,13,15,17,19,20,22,24,26,28,30,31,33,35,37,39};
-	
+
 	//Territory related information.
-	String territorydescriptions[] = new String[11];
-	String territorysubtext[] = new String[11];
 	int territoryPrice[] = {1000,1500,2000,3000,4000,4300,4750,5000,5500,6000,8000};
 	int territoryRent[] = {100,300,500,700,1000,1300,1600,2000,2600,3200,4000};
-	
+
+	//Labor Camp related information.
+	int laborCampPrice[] = {2500,2500};
+	int laborCampBaseRent[] = {100,100};
+
+	//Fleet related information.
+	int fleetPrice[] = {4000,4000,4000,4000};
+
+	//Tax related information.
+	int tax_TaxAmount[] = {2000,4000};
+	int tax_TaxRate[] = {0,10};
+
+	//Refuge related information.
+	int refugeRecieve[] = {5000,500};
+
+
+	//Field arrays:
 	//Territory fields.
 	Territory territoryFields[] = new Territory[11];
-	
-	
+	Fleet fleetFields[] = new Fleet[4];
+	Tax taxFields[] = new Tax[2];
+	LaborCamp laborCampFields[] = new LaborCamp[2];
+	Refuge refugeFields[] = new Refuge[2];
+
+
+
+
+
+
+	/**
+	 * Initializes the territory fields
+	 */
 	public void initTerritoryFields()
 	{
 		for(int i=0;i<territoryFields.length;i++)
 		{
 			String description = stringbanks.Territory_Stringbank.getTerritoryDescriptionsArray(i);
-			String subtext = TerritoryStringBank.getSubtextDescriptionsArray(i);
+			String subtext = stringbanks.Territory_Stringbank.getTerritorySubtextArray(i);
 			int price = territoryPrice[i];
 			int rent = territoryRent[i];
 			territoryFields[i] = new Territory(description,subtext,rent,price);
 		}
 	}
-//			public Territory(String descr, String subtext,int price, int rent)
-	Territory a = new Territory("descr","subtext",30,40);
-	
-	
 
-//	public void opretfields() 
-//	{
-//		for()
-//	}
-//	//Array {100,300,500..
-//	//pris	{1000,1500.
+	/**
+	 * initalizes the fleet fields
+	 */
+	public void initFleetFields()
+	{
+		for(int i=0;i<fleetFields.length;i++)
+		{
+			String description = stringbanks.Fleet_Stringbank.getFleetDescriptionArray(i);
+			String subtext = stringbanks.Fleet_Stringbank.getFleetSubtextArray(i);
+			int price = fleetPrice[i];
+			fleetFields[i] = new Fleet(description,subtext,price);
+		}
+	}
+	
+	/**
+	 * initoalizes the tax fields
+	 */
+	public void initTaxFields()
+	{
+		for(int i=0;i<taxFields.length;i++)
+		{
+			String description = stringbanks.Tax_Stringbank.getTaxDescriptionArray(i);
+			String subtext = stringbanks.Tax_Stringbank.getTaxSubtextArray(i);
+			int taxAmount = tax_TaxAmount[i];
+			int taxRate = tax_TaxRate[i];
+			taxFields[i] = new Tax(description,subtext,taxAmount,taxRate);
+		}
+	}
+	
+	/**
+	 * initializes the labor camp fields
+	 */
+	public void initLaborCampFields()
+	{
+		for(int i=0;i<laborCampFields.length;i++)
+		{
+			String description = stringbanks.LaborCamp_Stringbank.getLaborCampDescriptionArray(i);
+			String subtext = stringbanks.LaborCamp_Stringbank.getLaborCampSubtextArray(i);
+			int price = laborCampPrice[i];
+			int baseRent = laborCampBaseRent[i];
+			laborCampFields[i] = new LaborCamp(description,subtext,price,baseRent);
+		}
+	}
+	
+	/**
+	 * initializes the refuge fields
+	 */
+	public void initRefugeFields() 
+	{
+		for(int i=0;i<taxFields.length;i++)
+		{
+			String description = stringbanks.Refuge_Stringbank.getRefugeDescriptionArray(i);
+			String subtext = stringbanks.Refuge_Stringbank.getRefugeSubtextArray(i);
+			int recieve = refugeRecieve[i];
+			refugeFields[i] = new Refuge(description,subtext,recieve);
+		}
+	}
+
+
+
+
+
+	//	public void opretfields() 
+	//	{
+	//		for()
+	//	}
+	//	//Array {100,300,500..
+	//	//pris	{1000,1500.
 
 
 	/**
@@ -69,7 +161,7 @@ public class Board {
 				bgColors[i]=Color.YELLOW;
 				fieldNrReached++;
 			}
-			
+
 		}
 	}
 
@@ -79,16 +171,16 @@ public class Board {
 	 */
 	public void initfgcolor()
 	{	int fieldNrReached = 0;
-		for(int i = 0;i<40;i++)
+	for(int i = 0;i<40;i++)
+	{
+		fgColors[i]=Color.BLACK;
+		if(fieldsInUse[fieldNrReached]==i)
 		{
-			fgColors[i]=Color.BLACK;
-			if(fieldsInUse[fieldNrReached]==i)
-			{
-				fgColors[i]=Color.GREEN;
-				fieldNrReached++;
-			}
-			
+			fgColors[i]=Color.GREEN;
+			fieldNrReached++;
 		}
+
+	}
 	}
 
 
--- 42_CDIO_03/src/stringbanks/Territory_Stringbank.java
+++ 42_CDIO_03/src/stringbanks/Territory_Stringbank.java
@@ -3,31 +3,31 @@ package stringbanks;
 public class Territory_Stringbank {
 
 	//Territory descriptions.
-	String tribeEncampmentDescription = "Description";
-	String craterDescription = "Description";
-	String mountainDescription = "Description";
-	String coldDesertDescription = "Description";
-	String blackCaveDescription = "Description";
-	String wereWallDescription = "Description";
-	String mountainVillageDescription = "Description";
-	String southCitadelDescription = "Description";
-	String palaceGatesDescription = "Description";
-	String towerDescription = "Description";
-	String castleDescription = "Description";
+	private static String tribeEncampmentDescription = "Description";
+	private static String craterDescription = "Description";
+	private static String mountainDescription = "Description";
+	private static String coldDesertDescription = "Description";
+	private static String blackCaveDescription = "Description";
+	private static String wereWallDescription = "Description";
+	private static String mountainVillageDescription = "Description";
+	private static String southCitadelDescription = "Description";
+	private static String palaceGatesDescription = "Description";
+	private static String towerDescription = "Description";
+	private static String castleDescription = "Description";
 	
 	
 	//Territory subtext.
-	String tribeEncampmentSubtext = "Subtext";
-	String craterSubtext = "Subtext";
-	String mountainSubtext = "Subtext";
-	String coldDesertSubtext = "Subtext";
-	String blackCaveSubtext = "Subtext";
-	String wereWallSubtext = "Subtext";
-	String mountainVillageSubtext = "Subtext";
-	String southCitadelSubtext = "Subtext";
-	String palaceGatesSubtext = "Subtext";
-	String towerSubtext = "Subtext";
-	String castleSubtext = "Subtext";
+	private static String tribeEncampmentSubtext = "Subtext";
+	private static String craterSubtext = "Subtext";
+	private static String mountainSubtext = "Subtext";
+	private static String coldDesertSubtext = "Subtext";
+	private static String blackCaveSubtext = "Subtext";
+	private static String wereWallSubtext = "Subtext";
+	private static String mountainVillageSubtext = "Subtext";
+	private static String southCitadelSubtext = "Subtext";
+	private static String palaceGatesSubtext = "Subtext";
+	private static String towerSubtext = "Subtext";
+	private static String castleSubtext = "Subtext";
 	
 	
 	
@@ -36,21 +36,21 @@ public class Territory_Stringbank {
 	
 	
 	//Territory descriptions array
-	String territoryDescriptionsArray[] = {tribeEncampmentDescription,craterDescription,mountainDescription,coldDesertDescription,
+	private static String territoryDescriptionsArray[] = {tribeEncampmentDescription,craterDescription,mountainDescription,coldDesertDescription,
 			blackCaveDescription,wereWallDescription,mountainVillageDescription,southCitadelDescription,palaceGatesDescription,
 			towerDescription,castleDescription};
 	
 	//Territory subtext array
-	String territorySubtextArray[] = {tribeEncampmentSubtext,craterSubtext,mountainSubtext,coldDesertSubtext,blackCaveSubtext,
+	private static String territorySubtextArray[] = {tribeEncampmentSubtext,craterSubtext,mountainSubtext,coldDesertSubtext,blackCaveSubtext,
 			wereWallSubtext,mountainVillageSubtext,southCitadelSubtext,palaceGatesSubtext,towerSubtext,castleSubtext};
 
 	
 	//Getters 
-	public String getTerritoryDescriptionsArray(int i) {
+	public static String getTerritoryDescriptionsArray(int i) {
 		return territoryDescriptionsArray[i];
 	}
 
-	public String getTerritorySubtextArray(int i) {
+	public static String getTerritorySubtextArray(int i) {
 		return territorySubtextArray[i];
 	}
 	
