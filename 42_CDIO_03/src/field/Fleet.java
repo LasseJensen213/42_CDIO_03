c935ff2018fb65e50be918e090325dec29ec524e
lasse <lassejensen213@gmail.com>
2016-11-18 18:45:56 +0100

    Field Array complete

--- 42_CDIO_03/src/field/Fleet.java
+++ 42_CDIO_03/src/field/Fleet.java
@@ -1,5 +1,25 @@
 package field;
 
-public class Fleet {
+import player.Player;
 
+public class Fleet extends Ownable{
+	
+	public Fleet(String descr, String subtext,int price)
+	{
+		super(descr,subtext,price);
+		
+	}
+
+	@Override
+	public int getRent() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	void landOnField(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
