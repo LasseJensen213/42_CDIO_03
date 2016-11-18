e0588475b3b520b4ba66d335796427e5bcae1f5f
Jimmy <Jimmy@Jimmy-Pc>
2016-11-18 19:48:31 +0100

    Working on GameController

--- 42_CDIO_03/src/game/GameController.java
+++ 42_CDIO_03/src/game/GameController.java
@@ -1,5 +1,31 @@
 package game;
+import dice.DiceManager;
+import player.PlayerManager;
+import board.Board;
 
 public class GameController {
+	
+	private PlayerManager playerManager;
+	private DiceManager diceCup;
+	private Board gameBoard;
+	private int pNr; //Hvis tur det er.
+	
+	public GameController()
+	{
+		playerManager = new PlayerManager();
+		diceCup = new DiceManager(2,6);
+		gameBoard = new Board();
+		pNr = 0;
+		
+	}
+	
+	public void controller()
+	{
+		//Mangler en menu her//
+		
+		gameBoard.initBoard()//(Laver fields arrayet) Hvad end metoden nu kommer til at hedde
+		playerManager.initPlayers(); //Laver alle spillerne
+		
+	}
 	
 }
