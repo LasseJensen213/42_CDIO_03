6776c67c5ab5edc484a08ffe7b98912ba775d4a1
Jimmy <Jimmy@Jimmy-Pc>
2016-11-18 15:46:38 +0100

    Updated Player

--- 42_CDIO_03/src/player/Player.java
+++ 42_CDIO_03/src/player/Player.java
@@ -6,12 +6,16 @@ public class Player
 {
 	private String name;
 	private Account account;
-	private boolean skipTurn = false;
+	private boolean broke;
 	private int playerPos;
+	private int nFleetsOwned;
+	private int nLaborOwned;
+	private int lastDiceResult; // Bruges til labor camp
 
 	public Player(String name)
 	{
 		this.name = name;
+		this.broke = false;
 		Account account = new Account(30000);
 		this.playerPos = 1;
 	}
@@ -32,12 +36,12 @@ public class Player
 		this.account = account;
 	}
 
-	public boolean isSkipTurn() {
-		return skipTurn;
+	public boolean isBroke() {
+		return broke;
 	}
 
-	public void setSkipTurn(boolean skipTurn) {
-		this.skipTurn = skipTurn;
+	public void setSkipTurn(boolean broke) {
+		this.broke = broke;
 	}
 
 	public int getPlayerPos() {
@@ -46,6 +50,36 @@ public class Player
 
 	public void setPlayerPos(int playerPos) {
 		this.playerPos = playerPos;
+	}
+	
+	public int getFleetsOwned()
+	{
+		return nFleetsOwned;
+	}
+	
+	public int getLaborOwned()
+	{
+		return nLaborOwned;
+	}
+	
+	public void setFleetsOwned(int nFleetsOwned)
+	{
+		this.nFleetsOwned = nFleetsOwned;
+	}
+	
+	public void setLaborOwned(int nLaborOwned)
+	{
+		this.nLaborOwned = nLaborOwned;
+	}
+	
+	public int getDiceResult()
+	{
+		return this.lastDiceResult;
+	}
+	
+	public void setDiceResult(int diceResult)
+	{
+		this.lastDiceResult = diceResult;
 	}
 	
 	
