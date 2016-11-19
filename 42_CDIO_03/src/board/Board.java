a52b37a98f9593b9d92a1f28d61fcd16522ec96d
lasse <lassejensen213@gmail.com>
2016-11-19 20:30:03 +0100

    Board updated

--- 42_CDIO_03/src/board/Board.java
+++ 42_CDIO_03/src/board/Board.java
@@ -63,7 +63,7 @@ public class Board {
 			
 			Color bgColor = FieldGenerator.getBgColors(i);
 			Color fgColor = FieldGenerator.getFgColors(i);
-			fields[i]= new Tax.Builder().setTitle(title).setDescription(descr).setSubText(subtext).
+			fields[i]= new Tax.Builder().setTitle(title).setDescription(subtext).setSubText(descr).
 					setBgColor(bgColor).setFgColor(fgColor).build();
 			
 		}
