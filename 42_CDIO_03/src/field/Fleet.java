c13f1a5755f3f60de04aee6af47777a1a7e414fb
lasse <lassejensen213@gmail.com>
2016-11-21 15:06:02 +0100

    Fleet successfully merged into code

--- 42_CDIO_03/src/field/Fleet.java
+++ 42_CDIO_03/src/field/Fleet.java
@@ -18,34 +18,43 @@ public class Fleet extends Ownable{
 	}
 
 	@Override
-	public void landOnField (Player player) 
+	public void landOnField (Player p) 
 	{
+		GUI.showMessage("You landed on fleet");
+	
 		Player owner = this.getOwner();
-
-		if(owner == null && super.getPrice() < player.getAccount().getBalance());{
-			String answer = GUI.getUserSelection("Do you want to buy this Fleet?  Price is:"+this.getPrice(), new String[] {"buy", "skip"});
-
-			if(answer.equals("buy"));{
-				this.setOwner(player);
-				player.getAccount().deposit(-this.getPrice());
-			}
-
+		if(owner==null && p.getAccount().getBalance()>super.getPrice())
+		{
+			//Hvis feltet ingen ejer har
+			//Skal have mulighed for at købe, hvis han køber bliver 
+			//Hvis feltet ingen ejer har og spilleren har penge nok
+			//Skal have mulighed for at købe
+			
+				String options[] = {"Buy","Skip"};
+				String input = GUI.getUserSelection("Message", options);
+				if(input.equals(options[0]))
+				{
+					this.setOwner(p);
+					int alreadyOwned = p.getFleetsOwned();
+					p.setFleetsOwned(alreadyOwned+1);
+					p.getAccount().withdraw(this.getPrice());
+				}
 
 		}
-		if(this.getOwner()!=player){
-			int FleetsOwned = player.getFleetsOwned();
+		else if(this.getOwner()!=p){
+			int FleetsOwned = this.getOwner().getFleetsOwned();
 
 			switch(FleetsOwned) {
-			case "1": FleetsOwned = 500 ;  
+			case 1: FleetsOwned = 500 ;  
 			break;
-			case "2": FleetsOwned = 1000 ;  
+			case 3: FleetsOwned = 1000 ;  
 			break;
-			case "3": FleetsOwned = 2000 ;  
+			case 2: FleetsOwned = 2000 ;  
 			break;
-			case "4": FleetsOwned = 4000 ;  
+			case 4: FleetsOwned = 4000 ;  
 			break;
 			}
-			player.getAccount().deposit(-FleetsOwned);
+			p.getAccount().withdraw(FleetsOwned);
 			this.getOwner().getAccount().deposit(FleetsOwned);
 		}
 
